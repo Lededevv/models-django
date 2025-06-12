2214af07e4de1b62d8d168d02e3db91ee29c402b
Valeriy Lebedev <vqlev.84@yandex.ru>
2025-06-12 07:35:10 +0400

    creating a project - creating your own migrations

---
+++ mc_donalds/models.py
@@ -0,0 +1,32 @@
+
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+
+director = 'DI'
+admin = 'AD'
+cook = 'CO'
+cashier = 'CA'
+cleaner = 'CL'
+
+POSITIONS = [
+    (director, 'Директор'),
+    (admin, 'Администратор'),
+    (cook, 'Повар'),
+    (cashier, 'Кассир'),
+    (cleaner, 'Уборщик')
+]
+
+class Staff(models.Model):
+    full_name = models.CharField(max_length = 255)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=cashier)
+    labor_contract = models.IntegerField()
+
+
+
